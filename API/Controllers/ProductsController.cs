f651dcc6216a53c2d8b60df4906250ace8fa5b59
rzamirzayev <rzatm@code.edu.az>
2023-09-18 23:13:16 +0400

    created productsExtensions and added sorting

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,9 +16,15 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm, string brands, string types)
         {
-            return await _context.Products.ToListAsync();
+            var query = _context.Products
+            .Sort(orderBy)
+            .Search(searchTerm)
+            .Filter(brands, types)
+            .AsQueryable();
+
+            return await query.ToListAsync();
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
