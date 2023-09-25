6a28af8e49e43a928d1fbafbd98a100c9cc62d18
rzamirzayev <rzatm@code.edu.az>
2023-09-25 13:36:06 +0400

    added orders controller and page

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -19,19 +19,19 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Order>>> GetOrders()
+        public async Task<ActionResult<List<OrderDto>>> GetOrders()
         {
             return await _context.Orders
-            .Include(o => o.OrderItems)
+            .ProjectOrderToOrderDto()
             .Where(x => x.BuyerId == User.Identity.Name)
             .ToListAsync();
         }
 
         [HttpGet("{id}", Name = "GetOrder")]
-        public async Task<ActionResult<Order>> GetOrder(int id)
+        public async Task<ActionResult<OrderDto>> GetOrder(int id)
         {
             return await _context.Orders
-            .Include(x => x.OrderItems)
+            .ProjectOrderToOrderDto()
             .Where(x => x.BuyerId == User.Identity.Name && x.Id == id)
             .FirstOrDefaultAsync();
         }
@@ -77,8 +77,10 @@ namespace API.Controllers
             _context.Baskets.Remove(basket);
             if (orderDto.SaveAddress)
             {
-                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
-                user.Address = new UserAddress
+                var user = await _context.Users
+                .Include(a => a.Address)
+                .FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
+                var address = new UserAddress
                 {
                     FullName = orderDto.ShippingAddress.FullName,
                     Address1 = orderDto.ShippingAddress.Address1,
@@ -88,7 +90,7 @@ namespace API.Controllers
                     Zip = orderDto.ShippingAddress.Zip,
                     Country = orderDto.ShippingAddress.Country,
                 };
-                _context.Update(user);
+                user.Address = address;
             }
             var result = await _context.SaveChangesAsync() > 0;
             if (result) return CreatedAtRoute("GetOrder", new { id = order.Id }, order.Id);
